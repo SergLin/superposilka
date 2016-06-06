b3b6541d795dc889c9a05ff34fb30e590cc8f37f
Sergey Linnik <sergiuslinnikus@gmail.com>
2016-06-06 20:05:26 +0300

    06.06

--- js/main.js
+++ js/main.js
@@ -7,8 +7,20 @@
 /*
  * Custom   образец для сборщика ->    //= partials/app.js
  */
+//= partials/lightslider.js
+
+
 
 $('[data-opener]').click(function(){
  $('[data-open="'+$(this).data('opener')+'"]').toggle();
  
+});
+
+$(document).ready(function() {
+    $('#adaptive').lightSlider({
+        adaptiveHeight:true,
+        item:1,
+        slideMargin:0,
+        loop:true
+    });
 });
